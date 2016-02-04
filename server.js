edbf42d01f1fb9cb46361f409b8e8f0d1ee4f8a8
truesoul <mr.summersky@gmx.de>
2016-02-04 10:14:06 +0100

    bin/www und reset_token entfernt

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var SampleApp = function() {
     self.app.use(function(req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, reset_token');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 
       next();
     });
