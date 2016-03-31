efcd5719a686cfb931eca02faeb1a5942d5d4272
truesoul <mr.summersky@gmx.de>
2016-03-31 07:46:49 +0200

    Login Form

--- public/components/controller/login/login.controller.js
+++ public/components/controller/login/login.controller.js
@@ -6,8 +6,10 @@
     loginController.$inject = ['$scope', '$location', 'LoginService', 'UtilService'];
 
     function loginController($scope, $location, LoginService, UtilService) {
-        $scope.username = "";
-        $scope.password = "";
+        var vm = this;
+
+        vm.username = "";
+        vm.password = "";
 
         function success(){
             $location.path('/chat');
@@ -19,7 +21,7 @@
 
         $scope.sendToServer = function () {
             if(!UtilService.isStringEmpty($scope.username) && !UtilService.isStringEmpty($scope.password)){
-                LoginService.login($scope.username, $scope.password, success, error);
+                LoginService.login(vm.username, vm.password, success, error);
             }
         }
 
